72e69897b43f932974b2407aa0721d9ba77d83f4
Hizbucodes <hizbusoftware@gmail.com>
2025-11-26 17:59:55 +0530

    Add project files.

---
+++ DevBloggie.Web/Models/Domain/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace DevBloggie.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+        public ICollection<BlogPost> BlogPosts { get; set; } = new List<BlogPost>();
+    }
+}
