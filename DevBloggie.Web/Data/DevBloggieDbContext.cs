1846427279e0ae2444f3f6bbbe7b3e72e53f3556
Hizbucodes <hizbusoftware@gmail.com>
2025-11-27 12:40:43 +0530

    add ef core migration and use sql server

---
+++ DevBloggie.Web/Data/DevBloggieDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DevBloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevBloggie.Web.Data
+{
+    public class DevBloggieDbContext : DbContext
+    {
+        public DevBloggieDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+       public DbSet<BlogPost> BlogPosts { get; set; }
+       public DbSet<Tag> Tags { get; set; }
+    
+    }
+}
--- DevBloggie.Web/Program.cs
+++ DevBloggie.Web/Program.cs
@@ -1,9 +1,17 @@
+using DevBloggie.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<DevBloggieDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevBloggieDbConnectionString")));
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
