86e50917cbe41a72a416b126594a6f71da18b197
Hizbucodes <hizbusoftware@gmail.com>
2025-12-10 14:39:34 +0530

    add multiple select tags for blog post

---
+++ DevBloggie.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,37 @@
+﻿using DevBloggie.Web.Models.ViewModels;
+using DevBloggie.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DevBloggie.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagRepository _tagRepository;
+        public AdminBlogPostsController(ITagRepository tagRepository)
+        {
+            this._tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            // Get tags from repository
+          var tags = await _tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostsRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostsRequest addBlogPostsRequest)
+        {
+
+            return RedirectToAction("Add");
+        }
+    }
+}
---
+++ DevBloggie.Web/Models/ViewModels/AddBlogPostsRequest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DevBloggie.Web.Models.ViewModels
+{
+    public class AddBlogPostsRequest
+    {
+        public string Heading { get; set; } = string.Empty;
+        public string PageTitle { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string ShortDescription { get; set; } = string.Empty;
+        public string FeaturedImageUrl { get; set; } = string.Empty;
+        public string UrlHandle { get; set; } = string.Empty;
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; } = string.Empty;
+        public bool Visible { get; set; }
+
+
+        // Display tags
+        public IEnumerable<SelectListItem> Tags { get; set; }
+        // Collect Tag
+        public string[] SelectedTags { get; set; } = Array.Empty<string>();
+    }
+}
