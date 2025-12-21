1a0c644aba34031c1c7d739f5b79e383b2ccd429
Hizbucodes <hizbusoftware@gmail.com>
2025-12-21 13:48:43 +0530

    Register User Functionality

---
+++ DevBloggie.Web/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using DevBloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevBloggie.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerViewModel.Username,
+                Email = registerViewModel.Email
+            };
+
+           var identityResult = await userManager.CreateAsync(identityUser, registerViewModel.Password);
+
+            if (identityResult.Succeeded) 
+            {
+                // assign this user the "User" role
+               var roleIdentityResult = await userManager.AddToRoleAsync(identityUser, "User");
+            
+                if(roleIdentityResult.Succeeded)
+                {
+                    // Show successfull notification
+                    return RedirectToAction("Register");
+                }
+                
+            }
+
+            // Show error notification
+            return View();
+        }
+    }
+}
